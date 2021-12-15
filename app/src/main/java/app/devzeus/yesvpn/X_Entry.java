c8aa7d9db8f3f5cdd596cd55630d7f4f3454085c
devzeus-ke <dev.zeus.ke@gmail.com>
2021-12-15 12:59:25 +0300

    13

---
+++ app/src/main/java/app/devzeus/yesvpn/X_Entry.java
@@ -0,0 +1,24 @@
+package app.devzeus.yesvpn;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class X_Entry implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) {
+        if(lpparam.packageName == "com.eclipse.gamonyapp"){
+            XposedHelpers.findAndHookMethod(
+                    "com.eclipse.gamonyapp.Fragments.OffersFragment", lpparam.classLoader,
+                    "vpn", new XC_MethodHook() {
+                        @Override
+                        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                            param.setResult(false);
+                            super.afterHookedMethod(param);
+                        }
+                    }
+            );
+        }
+    }
+}
